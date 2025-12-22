c7800ad96f719354592086d9063ea18583aa433e
appkit-framework <admin@appkit.biz>
2025-12-22 03:33:29 +0100

    Refactor Task with deferred run

--- src/Task.php
+++ src/Task.php
@@ -3,26 +3,40 @@
 namespace AppKit\Async;
 
 use AppKit\Async\Exception\CanceledException;
+use AppKit\Async\Exception\TaskException;
 
 use Throwable;
+use React\Promise\Deferred;
+use React\EventLoop\Loop;
 use function React\Async\async;
 use function React\Async\await;
 
 class Task {
-    public const RUNNING   = 0;
-    public const CANCELING = 1;
-    public const COMPLETED = 2;
-    public const CANCELED  = 3;
-    public const FAILED    = 4;
+    public const PENDING   = 0;
+    public const RUNNING   = 1;
+    public const CANCELING = 2;
+    public const COMPLETED = 3;
+    public const CANCELED  = 4;
+    public const FAILED    = 5;
+
+    private $task;
 
     private $status;
-    private $promise;
+    private $deferred;
+    private $callPromise;
 
     function __construct($task) {
-        $this -> status = self::RUNNING;
-        $this -> promise = async($task)();
+        $this -> task = $task;
 
-        $this -> promise -> then(function($result) {
+        $this -> status = self::PENDING;
+        $this -> deferred = new Deferred(function() {
+            if($this -> callPromise)
+                $this -> callPromise -> cancel();
+            else
+                throw new CanceledException();
+        });
+
+        $this -> deferred -> promise() -> then(function($result) {
             $this -> status = self::COMPLETED;
         }) -> catch(function(CanceledException $e) {
             $this -> status = self::CANCELED;
@@ -31,12 +45,42 @@ class Task {
         });
     }
 
-    public function getStatus() {
-        return $this -> status;
+    public function run(...$args) {
+        if($this -> status != self::PENDING)
+            throw new TaskException('Task is not in PENDING state');
+
+        $this -> status = self::RUNNING;
+        Loop::futureTick(function() use($args) {
+            $this -> callPromise = async($this -> task)(...$args);
+            $this -> deferred -> resolve($this -> callPromise);
+        });
+
+        return $this;
     }
 
-    public function getPromise() {
-        return $this -> promise;
+    public function cancel() {
+        if($this -> status == self::PENDING || $this -> status == self::RUNNING) {
+            $this -> status = self::CANCELING;
+            $this -> deferred -> promise() -> cancel();
+        }
+
+        return $this;
+    }
+
+    public function await() {
+        return await($this -> deferred -> promise());
+    }
+
+    public function join() {
+        try {
+            $this -> await();
+        } catch(Throwable $e) {}
+
+        return $this;
+    }
+
+    public function getStatus() {
+        return $this -> status;
     }
 
     public function isActive() {
@@ -46,20 +90,5 @@ class Task {
 
     public function isDone() {
         return $this -> status >= self::COMPLETED;
-    }
-
-    public function await() {
-        return await($this -> promise);
-    }
-
-    public function join() {
-        try {
-            await($this -> promise);
-        } catch(Throwable $e) {}
-    }
-
-    public function cancel() {
-        $this -> status = self::CANCELING;
-        $this -> promise -> cancel();
     }
 }
